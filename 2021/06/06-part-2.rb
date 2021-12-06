163c0ba2297729cd65bc60dee3c6e279adb12e95
Amanda Lee-Zuck <a.leezuck@gmail.com>
2021-12-06 13:27:22 -0500

    2021 Day 6 solved.

---
+++ 2021/06/06-part-2.rb
@@ -0,0 +1,23 @@
+file = File.read("input.txt")
+input = file.split(",").map { |x| x.to_i }
+
+lanternfish = Hash.new(0)
+
+input.each do |num|
+  lanternfish[num] += 1
+end
+
+256.times do
+  new_hash = Hash.new(0)
+  lanternfish.each do |num, amount|
+    if num == 0
+      new_hash[8] += amount
+      new_hash[6] += amount
+    elsif (1..8).include?(num)
+      new_hash[num-1] += amount
+    end
+  end
+  lanternfish = new_hash
+end
+
+puts lanternfish.values.sum
